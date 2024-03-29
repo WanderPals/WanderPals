405a12d3808dd6202e1040662ea9d5a99778bbcc
mtv47 <m.tymchenko@protonmail.ch>
2024-03-29 08:30:46 +0100

    Added CREATE_TRIP Route

--- app/src/main/java/com/github/se/wanderpals/ui/navigation/Route.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/navigation/Route.kt
@@ -2,6 +2,7 @@ package com.github.se.wanderpals.ui.navigation
 
 /** Object defining the routes in the app. */
 object Route {
+  const val CREATE_TRIP = "create_trip"
   const val NOTIFICATION = "notif"
   const val DASHBOARD = "dashboard"
   const val AGENDA = "agenda"
