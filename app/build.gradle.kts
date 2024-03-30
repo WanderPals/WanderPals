81bb166ba21c7a4dc5c92c2df94506dbf42def8d
Paul Marchi <paul.marchi@epfl.ch>
2024-03-30 20:15:15 +0100

    Add the Map API, with a button to modify the display of the zoom button (Edit).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,16 +11,11 @@ plugins {
 
 }
 secrets {
-    // Optionally specify a different file name containing your secrets.
     // The plugin defaults to "local.properties"
     propertiesFileName = "secrets.properties"
 
-    // A properties file containing default secret values. This file can be
-    // checked in version control.
     defaultPropertiesFileName = "local.properties"
 
-    // Configure which keys should be ignored by the plugin by providing regular expressions.
-    // "sdk.dir" is ignored by default.
     ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
     ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
 }
--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/Map.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/Map.kt
@@ -3,23 +3,16 @@ package com.github.se.wanderpals.ui.screens.trip
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
 
 /** The Map screen. */
 @Composable
