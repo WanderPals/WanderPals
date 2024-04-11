3fa74d52a337b01e7013c9b83c70d2c3a6c12356
AgeX21 <70447855+AgeX21@users.noreply.github.com>
2024-04-11 21:40:39 +0200

    removed preview + ktfmt

--- app/src/main/java/com/github/se/wanderpals/ui/screens/dashboard/DashboardSuggestion.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/dashboard/DashboardSuggestion.kt
@@ -10,15 +10,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.github.se.wanderpals.model.data.GeoCords
-import com.github.se.wanderpals.model.data.Stop
 import com.github.se.wanderpals.model.data.Suggestion
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
@@ -55,32 +50,4 @@ fun DashboardSuggestion(suggestion: Suggestion) {
               }
         }
   }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DashboardSuggestionPreview() {
-  val stop =
-      Stop(
-          stopId = "1",
-          title = "Stop Title",
-          address = "123 Street",
-          date = LocalDate.now(),
-          startTime = LocalTime.now(),
-          duration = 60,
-          budget = 100.0,
-          description =
-              "This is a description of the stop. It should be long enough to wrap to the next line. and maybe even more. and even more",
-          geoCords = GeoCords(0.0, 0.0),
-          website = "https://example.com",
-          imageUrl = "")
-  val suggestion =
-      Suggestion(
-          suggestionId = "1",
-          userName = "User",
-          createdAt = LocalDate.now(),
-          stop = stop,
-          text = "This is a suggestion for a stop.",
-          userId = "1")
-  DashboardSuggestion(suggestion = suggestion)
 }
