2f160010604cd708d9bdad02651701e8de7cd75e
annhit29 <ann20010929@gmail.com>
2024-04-11 20:09:33 +0200

    add fake view model of tests

---
+++ app/src/androidTest/java/com/github/se/wanderpals/screens/CreateSuggestionScreen.kt
@@ -0,0 +1,24 @@
+package com.github.se.wanderpals.screens
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.KNode
+
+class CreateSuggestionScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<CreateSuggestionScreen>(
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag("createSuggestionScreen") }) {
+
+    val createButton: KNode = onNode { hasTestTag("createSuggestionButton") }
+    val goBackButton: KNode = onNode { hasTestTag("goBackButton") }
+
+    val inputTitle: KNode = onNode { hasTestTag("inputSuggestionTitle") }
+    val inputBudget: KNode = onNode { hasTestTag("inputSuggestionBudget") }
+    val inputStartDate: KNode = onNode { hasTestTag("inputSuggestionStartDate") }
+    val inputStartTime: KNode = onNode { hasTestTag("inputSuggestionStartTime") }
+    val inputEndDate: KNode = onNode { hasTestTag("inputSuggestionEndDate") }
+    val inputEndTime: KNode = onNode { hasTestTag("inputSuggestionEndTime") }
+    val inputDescription: KNode = onNode { hasTestTag("inputSuggestionDescription") }
+    val inputAddress: KNode = onNode { hasTestTag("inputSuggestionAddress") }
+    val inputWebsite: KNode = onNode { hasTestTag("inputSuggestionWebsite") }
+}
---
+++ app/src/main/java/com/github/se/wanderpals/model/viewmodel/CreateSuggestionViewModel.kt
@@ -0,0 +1,20 @@
+package com.github.se.wanderpals.model.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.se.wanderpals.model.data.Suggestion
+import com.github.se.wanderpals.model.repository.TripsRepository
+import kotlinx.coroutines.launch
+
+open class CreateSuggestionViewModel(tripsRepository: TripsRepository) : ViewModel() {
+
+    private val _tripsRepository = tripsRepository
+
+    open fun addSuggestion(tripId: String, suggestion: Suggestion): Boolean {
+        var a: Boolean = true
+        viewModelScope.launch {
+            _tripsRepository.addSuggestionToTrip(tripId, suggestion).also { a = it }
+        }
+        return a
+    }
+}
