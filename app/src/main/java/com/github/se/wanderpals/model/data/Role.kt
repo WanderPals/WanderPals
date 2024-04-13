f805b235b8a5811c12d63acdadcf29055692ecb7
tosigame <tosigame@gmail.com>
2024-04-13 17:39:03 +0200

    Created Role Enum, And updated User data class fields

---
+++ app/src/main/java/com/github/se/wanderpals/model/data/Role.kt
@@ -0,0 +1,9 @@
+package com.github.se.wanderpals.model.data
+
+/**
+ * Enum representing different roles a user can have within the application.
+ * Each role has different levels of access and permissions.
+ */
+enum class Role {
+    OWNER, ADMIN, MEMBER, VIEWER
+}
