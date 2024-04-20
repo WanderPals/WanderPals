b87b6ae3da30b5b74d250ae2d7eb3932a55576c8
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-04-20 13:35:35 +0200

    Done UI for top menu of notification screen

--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/notifications/Notifications.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/notifications/Notifications.kt
@@ -1,6 +1,6 @@
 package com.github.se.wanderpals.ui.screens.trip.notifications
 
-import androidx.compose.foundation.background
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,33 +12,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -46,8 +39,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.github.se.wanderpals.model.data.TripNotification
 import com.github.se.wanderpals.model.viewmodel.NotificationsViewModel
-import com.github.se.wanderpals.ui.navigation.Route
-import com.github.se.wanderpals.ui.screens.overview.shareTripCodeIntent
 import java.time.format.DateTimeFormatter
 
 /** The Notification screen. */
@@ -55,30 +46,83 @@ import java.time.format.DateTimeFormatter
 fun Notification(notificationsViewModel: NotificationsViewModel) {
 
     val notificationsList by notificationsViewModel.notifStateList.collectAsState()
+    var notificationSelected by remember { mutableStateOf(true) }
+
 
     Column {
-        // Header
-        Row(
+
+        Surface(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(100.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+                .height(100.dp)
+                .padding(horizontal = 16.dp, vertical = 22.dp),
+            shape = RoundedCornerShape(70.dp),
+            color = Color(0xFFA5B2C2)
         ) {
-            Text(text = "Notifications")
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { notificationSelected = true },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (notificationSelected) Color(0xFF5A7BF0) else Color.Transparent,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "Notifications",
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontWeight = if (notificationSelected) FontWeight.Bold else FontWeight.Normal
+                        )
+                    )
+                }
+                Button(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { notificationSelected = false },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (!notificationSelected) Color(0xFF5A7BF0) else Color.Transparent,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "Announcement",
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontWeight = if (!notificationSelected) FontWeight.Bold else FontWeight.Normal
+                        )
+                    )
+                }
+            }
         }
-        HorizontalDivider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
+
+
+
+        HorizontalDivider(color = Color.Black, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .weight(1f)
         ) {
             items(notificationsList) { notification ->
-                if(notification == notificationsList.first()){
-                    HorizontalDivider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
+                if (notification == notificationsList.first()) {
+                    HorizontalDivider(
+                        color = Color.Gray,
+                        thickness = 1.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    )
                 }
                 NotificationItem(notification = notification)
-                HorizontalDivider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
+                HorizontalDivider(
+                    color = Color.Gray,
+                    thickness = 1.dp,
+                    modifier = Modifier.fillMaxWidth()
+                )
 
             }
         }
@@ -86,19 +130,31 @@ fun Notification(notificationsViewModel: NotificationsViewModel) {
 }
 
 @Composable
-fun NotificationItem(notification : TripNotification){
-    Box(modifier = Modifier.fillMaxWidth().height(80.dp)) {
+fun NotificationItem(notification: TripNotification) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+    ) {
         Button(
             onClick = { },
             shape = RectangleShape,
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)) {
-            Row(modifier = Modifier.fillMaxSize().align(Alignment.CenterVertically)) {
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.CenterVertically)
+            ) {
                 // Title Text
                 Text(
                     text = notification.title,
                     style = TextStyle(fontSize = 16.sp),
                     color = Color.Black,
-                    modifier = Modifier.weight(1f).fillMaxWidth().align(Alignment.CenterVertically),
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .align(Alignment.CenterVertically),
                     textAlign = TextAlign.Start
                 )
 
