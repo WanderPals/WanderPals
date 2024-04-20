b1f303a49a9bfd48e8414d0fe7e41ebdc065377f
mtv47 <m.tymchenko@protonmail.ch>
2024-04-21 00:03:19 +0200

    Refactored tests

--- app/src/androidTest/java/com/github/se/wanderpals/trip/TripNavigationTest.kt
+++ app/src/androidTest/java/com/github/se/wanderpals/trip/TripNavigationTest.kt
@@ -1,12 +1,18 @@
 package com.github.se.wanderpals.trip
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.github.se.wanderpals.BuildConfig
 import com.github.se.wanderpals.model.repository.TripsRepository
 import com.github.se.wanderpals.screens.TripScreen
 import com.github.se.wanderpals.ui.navigation.NavigationActions
+import com.github.se.wanderpals.ui.navigation.NavigationActionsVariables
+import com.github.se.wanderpals.ui.navigation.Route
+import com.github.se.wanderpals.ui.navigation.globalVariables
+import com.github.se.wanderpals.ui.navigation.rememberMultiNavigationAppState
 import com.github.se.wanderpals.ui.screens.trip.Trip
 import com.google.android.libraries.places.api.Places
 import com.kaspersky.components.composesupport.config.withComposeSupport
@@ -27,11 +33,26 @@ class TripNavigationTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCom
 
   @get:Rule val mockkRule = MockKRule(this)
 
+  @RelaxedMockK lateinit var mockNavController: NavHostController
+
+  @RelaxedMockK lateinit var mockNavController2: NavHostController
+
   @RelaxedMockK lateinit var mockNavActions: NavigationActions
 
   @Before
   fun testSetup() {
     composeTestRule.setContent {
+      mockNavController = rememberNavController()
+      mockNavController2 = rememberNavController()
+      globalVariables = NavigationActionsVariables()
+      mockNavActions =
+          NavigationActions(
+              mainNavigation =
+                  rememberMultiNavigationAppState(
+                      startDestination = Route.ROOT_ROUTE, mockNavController),
+              tripNavigation =
+                  rememberMultiNavigationAppState(
+                      startDestination = Route.DASHBOARD, mockNavController2))
       val context = InstrumentationRegistry.getInstrumentation().targetContext
       Places.initialize(context, BuildConfig.MAPS_API_KEY)
       val placesClient = Places.createClient(context)
