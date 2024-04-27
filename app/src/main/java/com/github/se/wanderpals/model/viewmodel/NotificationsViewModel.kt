429c1639c5d35ff7cc65e9f757477675466eb375
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-04-27 13:17:04 +0200

    ktfm format

--- app/src/main/java/com/github/se/wanderpals/model/viewmodel/NotificationsViewModel.kt
+++ app/src/main/java/com/github/se/wanderpals/model/viewmodel/NotificationsViewModel.kt
@@ -21,92 +21,86 @@ import kotlinx.coroutines.runBlocking
 open class NotificationsViewModel(val tripsRepository: TripsRepository, val tripId: String) :
     ViewModel() {
 
-    /* States */
-    private val _notifStateList = MutableStateFlow(emptyList<TripNotification>())
-    open val notifStateList: StateFlow<List<TripNotification>> = _notifStateList
+  /* States */
+  private val _notifStateList = MutableStateFlow(emptyList<TripNotification>())
+  open val notifStateList: StateFlow<List<TripNotification>> = _notifStateList
 
-    private val _announcementStateList = MutableStateFlow(emptyList<Announcement>())
-    open val announcementStateList: StateFlow<List<Announcement>> = _announcementStateList
+  private val _announcementStateList = MutableStateFlow(emptyList<Announcement>())
+  open val announcementStateList: StateFlow<List<Announcement>> = _announcementStateList
 
-    private val _isLoading = MutableStateFlow(true)
-    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+  private val _isLoading = MutableStateFlow(true)
+  val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
-    private val _isNotifSelected = MutableStateFlow(true)
-    val isNotifSelected: StateFlow<Boolean> = _isNotifSelected.asStateFlow()
+  private val _isNotifSelected = MutableStateFlow(true)
+  val isNotifSelected: StateFlow<Boolean> = _isNotifSelected.asStateFlow()
 
-    private val _announcementItemPressed = MutableStateFlow(false)
-    val announcementItemPressed: StateFlow<Boolean> = _announcementItemPressed.asStateFlow()
+  private val _announcementItemPressed = MutableStateFlow(false)
+  val announcementItemPressed: StateFlow<Boolean> = _announcementItemPressed.asStateFlow()
 
-    private val _selectedAnnouncementId = MutableStateFlow("")
-    val selectedAnnouncementID: StateFlow<String> = _selectedAnnouncementId.asStateFlow()
+  private val _selectedAnnouncementId = MutableStateFlow("")
+  val selectedAnnouncementID: StateFlow<String> = _selectedAnnouncementId.asStateFlow()
 
-    /**
-     * Updates the state lists of notifications and announcements by launching a coroutine within the viewModel scope.
-     *
-     */
-    open fun updateStateLists() {
-        viewModelScope.launch {
-            // Set loading state to true before fetching data
-            _isLoading.value = true
-            // Fetch all trips from the repository
-            _announcementStateList.value =
-                tripsRepository
-                    .getAllAnnouncementsFromTrip(tripId)
-                    .reversed()
-            _notifStateList.value = tripsRepository.getNotificationList(tripId)
-            // Set loading state to false after data is fetched
-            _isLoading.value = false
-        }
+  /**
+   * Updates the state lists of notifications and announcements by launching a coroutine within the
+   * viewModel scope.
+   */
+  open fun updateStateLists() {
+    viewModelScope.launch {
+      // Set loading state to true before fetching data
+      _isLoading.value = true
+      // Fetch all trips from the repository
+      _announcementStateList.value = tripsRepository.getAllAnnouncementsFromTrip(tripId).reversed()
+      _notifStateList.value = tripsRepository.getNotificationList(tripId)
+      // Set loading state to false after data is fetched
+      _isLoading.value = false
     }
+  }
 
-    /**
-     * Adds a Announcement by the administrator to a trip.
-     *
-     * @param announcement The Announcement object to be added.
-     * @return A boolean representing the success of the operation.
-     */
-    open fun addAnnouncement(announcement: Announcement) {
-        runBlocking { tripsRepository.addAnnouncementToTrip(tripId, announcement) }
+  /**
+   * Adds a Announcement by the administrator to a trip.
+   *
+   * @param announcement The Announcement object to be added.
+   * @return A boolean representing the success of the operation.
+   */
+  open fun addAnnouncement(announcement: Announcement) {
+    runBlocking { tripsRepository.addAnnouncementToTrip(tripId, announcement) }
+  }
+
+  /**
+   * Removes a specific announcement from a trip.
+   *
+   * @param announcementId The ID of the announcement to remove.
+   */
+  open fun removeAnnouncement(announcementId: String) {
+    runBlocking {
+      tripsRepository.removeAnnouncementFromTrip(tripId, announcementId)
+      updateStateLists()
     }
+  }
 
-    /**
-     * Removes a specific announcement from a trip.
-     *
-     * @param announcementId The ID of the announcement to remove.
-     */
-    open fun removeAnnouncement(announcementId: String) {
-        runBlocking {
-            tripsRepository.removeAnnouncementFromTrip(tripId, announcementId)
-            updateStateLists()
-        }
+  /* Setter functions */
+  fun setNotificationSelectionState(value: Boolean) {
+    _isNotifSelected.value = value
+  }
+
+  fun setAnnouncementItemPressState(value: Boolean) {
+    _announcementItemPressed.value = value
+  }
+
+  fun setSelectedAnnouncementId(announcementId: String) {
+    _selectedAnnouncementId.value = announcementId
+  }
+
+  class NotificationsViewModelFactory(
+      private val tripsRepository: TripsRepository,
+      private val tripId: String
+  ) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      if (modelClass.isAssignableFrom(NotificationsViewModel::class.java)) {
+        @Suppress("UNCHECKED_CAST") return NotificationsViewModel(tripsRepository, tripId) as T
+      }
+      throw IllegalArgumentException("Unknown ViewModel class")
     }
-
-    /* Setter functions */
-    fun setNotificationSelectionState(value: Boolean) {
-        _isNotifSelected.value = value
-    }
-
-    fun setAnnouncementItemPressState(value: Boolean) {
-        _announcementItemPressed.value = value
-    }
-
-    fun setSelectedAnnouncementId(announcementId: String) {
-        _selectedAnnouncementId.value = announcementId
-    }
-
-    class NotificationsViewModelFactory(
-        private val tripsRepository: TripsRepository,
-        private val tripId: String
-    ) : ViewModelProvider.Factory {
-
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(NotificationsViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST") return NotificationsViewModel(
-                    tripsRepository,
-                    tripId
-                ) as T
-            }
-            throw IllegalArgumentException("Unknown ViewModel class")
-        }
-    }
+  }
 }
--- app/src/main/java/com/github/se/wanderpals/service/NotificationsManager.kt
+++ app/src/main/java/com/github/se/wanderpals/service/NotificationsManager.kt
@@ -6,74 +6,70 @@ import com.github.se.wanderpals.navigationActions
 import com.github.se.wanderpals.ui.navigation.Route
 import java.time.LocalDateTime
 
-/**
- * Singleton object responsible for managing trip notifications.
- */
+/** Singleton object responsible for managing trip notifications. */
 object NotificationsManager {
-    private lateinit var tripsRepository: TripsRepository
-    private const val MAX_NOTIF_SIZE = 20
+  private lateinit var tripsRepository: TripsRepository
+  private const val MAX_NOTIF_SIZE = 20
 
-    /**
-     * Initializes the NotificationsManager with the provided TripsRepository instance.
-     *
-     * @param repository The TripsRepository instance to be used for managing trip notifications.
-     */
-    fun initNotificationsManager(repository: TripsRepository) {
-        tripsRepository = repository
-    }
+  /**
+   * Initializes the NotificationsManager with the provided TripsRepository instance.
+   *
+   * @param repository The TripsRepository instance to be used for managing trip notifications.
+   */
+  fun initNotificationsManager(repository: TripsRepository) {
+    tripsRepository = repository
+  }
 
-    /**
-     * Adds a new notification to the list, maintaining a maximum size of [MAX_NOTIF_SIZE].
-     *
-     * @param notifList The current list of notifications.
-     * @param newNotif The new notification to be added.
-     * @return The updated list of notifications after adding the new one.
-     */
-    private fun addNewNotification(
-        notifList: MutableList<TripNotification>,
-        newNotif: TripNotification
-    ): List<TripNotification> {
-        if (notifList.size >= MAX_NOTIF_SIZE) {
-            notifList.removeLast()
-        }
-        notifList.add(0, newNotif)
-        return notifList.toList()
+  /**
+   * Adds a new notification to the list, maintaining a maximum size of [MAX_NOTIF_SIZE].
+   *
+   * @param notifList The current list of notifications.
+   * @param newNotif The new notification to be added.
+   * @return The updated list of notifications after adding the new one.
+   */
+  private fun addNewNotification(
+      notifList: MutableList<TripNotification>,
+      newNotif: TripNotification
+  ): List<TripNotification> {
+    if (notifList.size >= MAX_NOTIF_SIZE) {
+      notifList.removeLast()
     }
+    notifList.add(0, newNotif)
+    return notifList.toList()
+  }
 
-    /**
-     * Adds a notification indicating that a user has joined a trip.
-     *
-     * @param tripId The ID of the trip for which the notification is added.
-     */
-    suspend fun addJoinTripNotification(tripId: String) {
-        var notifList = tripsRepository.getNotificationList(tripId).toMutableList()
-        val newNotif =
-            TripNotification(
-                "${SessionManager.getCurrentUser()!!.name} joined the trip ",
-                Route.MEMBERS,
-                LocalDateTime.now()
-            )
-        addNewNotification(notifList, newNotif)
-        tripsRepository.setNotificationList(tripId, notifList.toList())
-    }
+  /**
+   * Adds a notification indicating that a user has joined a trip.
+   *
+   * @param tripId The ID of the trip for which the notification is added.
+   */
+  suspend fun addJoinTripNotification(tripId: String) {
+    var notifList = tripsRepository.getNotificationList(tripId).toMutableList()
+    val newNotif =
+        TripNotification(
+            "${SessionManager.getCurrentUser()!!.name} joined the trip ",
+            Route.MEMBERS,
+            LocalDateTime.now())
+    addNewNotification(notifList, newNotif)
+    tripsRepository.setNotificationList(tripId, notifList.toList())
+  }
 
-    /**
-     * Adds a notification indicating that a new suggestion has been created for a trip.
-     *
-     * @param tripId The ID of the trip for which the notification is added.
-     * @param suggestionId The ID of the newly created suggestion.
-     */
-    suspend fun addCreateSuggestionNotification(tripId: String, suggestionId: String) {
-        var notifList = tripsRepository.getNotificationList(tripId).toMutableList()
-        val navActions = navigationActions.copy()
-        navActions.setVariablesSuggestion(suggestionId)
-        val newNotif =
-            TripNotification(
-                "${SessionManager.getCurrentUser()!!.name} created a new suggestion ",
-                Route.SUGGESTION_DETAIL + "/" + navActions.serializeNavigationVariable(),
-                LocalDateTime.now()
-            )
-        addNewNotification(notifList, newNotif)
-        tripsRepository.setNotificationList(tripId, notifList.toList())
-    }
+  /**
+   * Adds a notification indicating that a new suggestion has been created for a trip.
+   *
+   * @param tripId The ID of the trip for which the notification is added.
+   * @param suggestionId The ID of the newly created suggestion.
+   */
+  suspend fun addCreateSuggestionNotification(tripId: String, suggestionId: String) {
+    var notifList = tripsRepository.getNotificationList(tripId).toMutableList()
+    val navActions = navigationActions.copy()
+    navActions.setVariablesSuggestion(suggestionId)
+    val newNotif =
+        TripNotification(
+            "${SessionManager.getCurrentUser()!!.name} created a new suggestion ",
+            Route.SUGGESTION_DETAIL + "/" + navActions.serializeNavigationVariable(),
+            LocalDateTime.now())
+    addNewNotification(notifList, newNotif)
+    tripsRepository.setNotificationList(tripId, notifList.toList())
+  }
 }
