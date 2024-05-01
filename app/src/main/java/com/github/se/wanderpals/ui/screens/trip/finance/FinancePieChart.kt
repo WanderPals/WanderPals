b5633c67a1f01b9e123b77928cea3c9c368ff3bb
AgeX21 <70447855+AgeX21@users.noreply.github.com>
2024-05-01 21:31:44 +0200

    forgot ktfmt

--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/FinancePieChart.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/FinancePieChart.kt
@@ -17,41 +17,48 @@ import com.github.se.wanderpals.model.data.Expense
 
 @Composable
 fun FinancePieChart(
-    expenses : List<Expense>,
-    radiusOuter : Dp = 100.dp,
-    chartBandWidth : Dp = 20.dp,
+    expenses: List<Expense>,
+    radiusOuter: Dp = 100.dp,
+    chartBandWidth: Dp = 20.dp,
 ) {
-    val pieChartData = expenses.groupBy { it.category }
-        .map { (category, expenses) -> category to expenses.sumOf { it.amount } }
-        .toMap()
+  val pieChartData =
+      expenses
+          .groupBy { it.category }
+          .map { (category, expenses) -> category to expenses.sumOf { it.amount } }
+          .toMap()
 
-    val totalExpense = pieChartData.values.sum()
-    val pieChartValues = pieChartData.values.toList().map { 360f * it.toFloat() / totalExpense.toFloat() }
+  val totalExpense = pieChartData.values.sum()
+  val pieChartValues =
+      pieChartData.values.toList().map { 360f * it.toFloat() / totalExpense.toFloat() }
 
-    val colors = listOf(
-        Color(0xFFFFC09F),
-        Color(0xFF2E5EAA),
-        Color(0xFFCF4D6F),
-        Color(0xFFA5F8D3),
-        Color(0xFF30BCED)) // Need to change this to prettier color but i don't know if hard coding is the best way to do it
+  val colors =
+      listOf(
+          Color(0xFFFFC09F),
+          Color(0xFF2E5EAA),
+          Color(0xFFCF4D6F),
+          Color(0xFFA5F8D3),
+          Color(
+              0xFF30BCED)) // Need to change this to prettier color but i don't know if hard coding
+  // is the best way to do it
 
-    var lastValue = -90f
+  var lastValue = -90f
 
-    Box(
-        modifier = Modifier.size(radiusOuter * 2f+chartBandWidth*2).testTag("FinancePieChart").background(Color.DarkGray),
-        contentAlignment = Alignment.Center
-    ) {
+  Box(
+      modifier =
+          Modifier.size(radiusOuter * 2f + chartBandWidth * 2)
+              .testTag("FinancePieChart")
+              .background(Color.DarkGray),
+      contentAlignment = Alignment.Center) {
         Canvas(modifier = Modifier.size(radiusOuter * 2f)) {
-            pieChartValues.forEachIndexed { index, fl ->
-                drawArc(
-                    color = colors[index],
-                    startAngle = lastValue,
-                    sweepAngle = fl-2,
-                    useCenter = false,
-                    style = Stroke(width = chartBandWidth.toPx(), cap = StrokeCap.Butt)
-                )
-                lastValue += fl
-            }
+          pieChartValues.forEachIndexed { index, fl ->
+            drawArc(
+                color = colors[index],
+                startAngle = lastValue,
+                sweepAngle = fl - 2,
+                useCenter = false,
+                style = Stroke(width = chartBandWidth.toPx(), cap = StrokeCap.Butt))
+            lastValue += fl
+          }
         }
-    }
+      }
 }
