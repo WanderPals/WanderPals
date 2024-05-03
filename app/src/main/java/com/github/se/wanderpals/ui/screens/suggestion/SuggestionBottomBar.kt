e80aecb6e2ef0f2a2c2cf969ea14ccfd3781c531
annhit29 <ann20010929@gmail.com>
2024-05-03 16:08:23 +0200

    createSuggestionUI

--- app/src/main/java/com/github/se/wanderpals/ui/screens/suggestion/SuggestionBottomBar.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/suggestion/SuggestionBottomBar.kt
@@ -1,16 +1,10 @@
 package com.github.se.wanderpals.ui.screens.suggestion
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -28,27 +22,25 @@ import com.github.se.wanderpals.ui.theme.primaryContainerLight
 @Composable
 fun SuggestionBottomBar(onSuggestionClick: () -> Unit = {}) {
 
-    // Button to create a suggestion
-    Box(Modifier.fillMaxWidth().padding(bottom = 16.dp)
-    ) { // Added padding to raise the button
-        Button(
-            onClick = { onSuggestionClick() },
-            modifier =
-            Modifier
-                .align(Alignment.BottomEnd) // Align the button to the bottom end of the screen
+  // Button to create a suggestion
+  Box(Modifier.fillMaxWidth().padding(bottom = 16.dp)) { // Added padding to raise the button
+    Button(
+        onClick = { onSuggestionClick() },
+        modifier =
+            Modifier.align(Alignment.BottomEnd) // Align the button to the bottom end of the screen
                 .padding(end = 27.dp) // Add padding to the end of the screen
                 .height(64.dp) // Set the height to 56.dp to make it a square
                 .testTag("suggestionButtonExists"),
-            shape = RoundedCornerShape(size = 16.dp),
-            colors = ButtonDefaults.buttonColors(
+        shape = RoundedCornerShape(size = 16.dp),
+        colors =
+            ButtonDefaults.buttonColors(
                 containerColor = primaryContainerLight, // Button color
                 contentColor = onPrimaryContainerLight, // Icon color
-            )
-        ) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                modifier = Modifier.size(24.dp),
-                contentDescription = null)
+            )) {
+          Icon(
+              imageVector = Icons.Default.Add,
+              modifier = Modifier.size(24.dp),
+              contentDescription = null)
         }
-    }
+  }
 }
