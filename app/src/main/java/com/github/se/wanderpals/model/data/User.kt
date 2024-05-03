cc907acdd347b441bbd468dc591e48e4e4a298d4
mtv47 <m.tymchenko@protonmail.ch>
2024-05-03 13:37:05 +0200

    Modified the description of the nickname

--- app/src/main/java/com/github/se/wanderpals/model/data/User.kt
+++ app/src/main/java/com/github/se/wanderpals/model/data/User.kt
@@ -5,9 +5,10 @@ package com.github.se.wanderpals.model.data
  * now includes additional fields such as nickname, lastPosition, and profilePictureURL.
  *
  * @param userId Unique identifier for the user.
- * @param name User's full name for display and identification.
+ * @param name User's full name for display and identification. User's name for casual display or
+ *   alternative identification.
  * @param email User's email address, used for communication and login.
- * @param nickname User's nickname for casual display or alternative identification.
+ * @param nickname User's nickname for identification
  * @param role User's role within the application, influencing access and capabilities.
  * @param lastPosition Geographic coordinates representing the user's last known position.
  * @param profilePictureURL URL to the user's profile picture.
