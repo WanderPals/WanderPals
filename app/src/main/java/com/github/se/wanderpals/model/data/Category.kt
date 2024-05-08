47b410c0815400eba377ac449ab631e4d6fe1e62
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-05-08 19:50:30 +0200

    Changed Transport category color

--- app/src/main/java/com/github/se/wanderpals/model/data/Category.kt
+++ app/src/main/java/com/github/se/wanderpals/model/data/Category.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 
 /** Categories of expenses that can be used to classify each expense record. */
 enum class Category(val nameToDisplay : String,val color : Color) {
-  TRANSPORT("Transport",Color(0xFF2E5EAA)),
+  TRANSPORT("Transport",Color(0xFF5B61D6)),
   ACCOMMODATION("Accomodation",Color(0xFFCF4D6F)),
   ACTIVITIES( "Activities",Color(0xFFAE59DC)),
   FOOD("Food", Color(0xFFFFA500)),
