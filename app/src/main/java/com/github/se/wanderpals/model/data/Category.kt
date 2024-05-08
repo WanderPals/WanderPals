43bf743109b996acda3fd25cf92ea1cd8ea31868
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-05-08 18:01:16 +0200

    changing category order to have intuitive display in category option of finance screen

--- app/src/main/java/com/github/se/wanderpals/model/data/Category.kt
+++ app/src/main/java/com/github/se/wanderpals/model/data/Category.kt
@@ -5,10 +5,9 @@ import androidx.compose.ui.graphics.Color
 
 /** Categories of expenses that can be used to classify each expense record. */
 enum class Category(val nameToDisplay : String,val color : Color) {
-  FOOD("Food", Color(0xFFFFC09F)),
   TRANSPORT("Transport",Color(0xFF2E5EAA)),
   ACCOMMODATION("Accomodation",Color(0xFFCF4D6F)),
   ACTIVITIES( "Activities",Color(0xFFAE59DC)),
+  FOOD("Food", Color(0xFFFFC09F)),
   OTHER("Other", Color(0xFF30BCED))
-
 }
