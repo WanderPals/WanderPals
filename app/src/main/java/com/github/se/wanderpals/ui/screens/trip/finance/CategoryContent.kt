ae6f6bd9e89aa029e983f6d51b16f20bb27a5f09
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-05-08 19:42:51 +0200

    adding display of the total expense value on the finance piechart

--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/CategoryContent.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/CategoryContent.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,7 +14,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,15 +23,12 @@ import com.github.se.wanderpals.model.data.Expense
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.se.wanderpals.model.data.Category
-import okhttp3.internal.notify
 import java.time.LocalDate
 
 fun createExpenses(): List<Expense> {
@@ -55,7 +49,7 @@ fun createExpenses(): List<Expense> {
     val expense2 = Expense(
         expenseId = "2",
         title = "Taxi",
-        amount = 2000000000000000000.0,
+        amount = 2000.0,
         category = Category.FOOD,
         userId = "user2",
         userName = "Jane",
@@ -114,7 +108,10 @@ fun CategoryContent(innerPadding: PaddingValues, expenseList: List<Expense>) {
                         .padding(vertical = 10.dp),
                     verticalArrangement = Arrangement.SpaceBetween,
                     horizontalAlignment = Alignment.CenterHorizontally) {
-                    FinancePieChart(expenses = expenseList, radiusOuter = 80.dp)
+                    FinancePieChart(
+                        expenses = expenseList,
+                        radiusOuter = 80.dp,
+                        totalValueDisplayIsEnabled = true)
 
                     Spacer(modifier = Modifier.height(20.dp))
                     Row(
--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/FinancePieChart.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/FinancePieChart.kt
@@ -1,16 +1,19 @@
 package com.github.se.wanderpals.ui.screens.trip.finance
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.github.se.wanderpals.model.data.Category
@@ -28,6 +31,7 @@ fun FinancePieChart(
     expenses: List<Expense>,
     radiusOuter: Dp = 100.dp,
     chartBandWidth: Dp = 20.dp,
+    totalValueDisplayIsEnabled : Boolean = false
 ) {
   val pieChartData =
       expenses
@@ -65,6 +69,23 @@ fun FinancePieChart(
                 style = Stroke(width = chartBandWidth.toPx(), cap = StrokeCap.Butt))
             lastValue += fl
           }
+        }
+        if(totalValueDisplayIsEnabled){
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Total Value",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+                Text(
+                    text = "$totalExpense CHF",
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }
         }
       }
 }
