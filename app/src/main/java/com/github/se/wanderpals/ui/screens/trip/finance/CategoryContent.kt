bcb54c77fa9b093c6782c474bf00f14c2f6fe2ad
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-05-08 17:52:28 +0200

    CategoryInfo item are displayed, need to fix order of display and add percentage info

--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/CategoryContent.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/CategoryContent.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,9 +24,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import com.github.se.wanderpals.model.data.Expense
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.se.wanderpals.model.data.Category
+import okhttp3.internal.notify
 import java.time.LocalDate
 
 fun createExpenses(): List<Expense> {
@@ -44,7 +55,7 @@ fun createExpenses(): List<Expense> {
     val expense2 = Expense(
         expenseId = "2",
         title = "Taxi",
-        amount = 20.0,
+        amount = 2000000000000000000.0,
         category = Category.FOOD,
         userId = "user2",
         userName = "Jane",
@@ -132,11 +143,14 @@ fun CategoryContent(innerPadding: PaddingValues, expenseList: List<Expense>) {
 
             }
         }
+
         items(Category.values()){ category ->
+            val categoryInfo = categoryTransactionMap.getValue(category)
+            Spacer(modifier = Modifier.height(20.dp))
             CategoryInfoItem(
                 category = category,
-                nbTransaction = categoryTransactionMap[category]!!.first,
-                totalCategoryAmount = categoryTransactionMap[category]!!.second)
+                nbTransaction = categoryInfo.first,
+                totalCategoryAmount = categoryInfo.second)
         }
 
     }
@@ -162,7 +176,58 @@ fun PieChartCategoryText(category: Category){
 }
 
 @Composable
-fun CategoryInfoItem(category : Category,nbTransaction : Int,totalCategoryAmount: Double){
+fun CategoryInfoItem(category : Category,nbTransaction : Int,totalCategoryAmount: Double) {
 
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        shape = RoundedCornerShape(10.dp),
+        color = MaterialTheme.colorScheme.secondaryContainer,
+    ) {
+        Row(
+            modifier =
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Box(
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                        .size(35.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(category.color)
 
+                )
+                Column {
+                    Text(
+                        modifier = Modifier.padding(start = 10.dp),
+                        text = category.nameToDisplay,
+                        style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = 10.dp),
+                        text = "$nbTransaction transactions" ,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray,
+                    )
+                }
+            }
+
+            Text(
+                modifier = Modifier.padding(end = 15.dp),
+                text = "%.2f CHF".format(totalCategoryAmount),
+                style =  MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.End,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+        }
+    }
 }
