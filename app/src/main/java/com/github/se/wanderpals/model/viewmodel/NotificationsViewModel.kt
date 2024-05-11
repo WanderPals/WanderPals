ab4f9eb351cab5e733979c78ca34f45b9cd79bbc
tosigame <tosigame@gmail.com>
2024-05-11 22:07:26 +0200

    ktft(pre test)

--- app/src/main/java/com/github/se/wanderpals/model/viewmodel/NotificationsViewModel.kt
+++ app/src/main/java/com/github/se/wanderpals/model/viewmodel/NotificationsViewModel.kt
@@ -1,6 +1,5 @@
 package com.github.se.wanderpals.model.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
