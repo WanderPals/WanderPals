44098b3ee01a771eb3fa23e5f84fd00825454668
Paul Marchi <paul.marchi@epfl.ch>
2024-05-13 16:26:42 +0200

    Fix of the clicked bug

---
+++ app/src/androidTest/java/com/github/se/wanderpals/screens/DocumentsPSScreen.kt
@@ -0,0 +1,11 @@
+package com.github.se.wanderpals.screens
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+
+class DocumentsPSScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<DocumentsPSScreen>(
+        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("DocumentsPSScreen") }) {
+
+
+        }
