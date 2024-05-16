150f403f1a478ca2321a1e2dbc0e156c5c5abca2
Gabin Forestier <gabin.forestier1007@gmail.com>
2024-05-16 15:35:14 +0200

    StopItemTest + StopItemViewModelTest done

--- app/src/androidTest/java/com/github/se/wanderpals/stops/StopItemTest.kt
+++ app/src/androidTest/java/com/github/se/wanderpals/stops/StopItemTest.kt
@@ -1,37 +1,128 @@
 package com.github.se.wanderpals.stops
 
+import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.se.wanderpals.model.viewmodel.StopItemViewModel
+import com.github.se.wanderpals.model.data.GeoCords
+import com.github.se.wanderpals.model.data.Role
+import com.github.se.wanderpals.model.data.Stop
+import com.github.se.wanderpals.model.repository.TripsRepository
+import com.github.se.wanderpals.service.SessionManager
+import com.github.se.wanderpals.ui.screens.trip.stops.StopItem
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.mockk.mockk
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import org.junit.Before
+import java.time.LocalDate
+import java.time.LocalTime
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-class FakeStopItemViewModel : StopItemViewModel(mockk(relaxed = true), tripId = "1") {
-  private val _isDeleted = MutableStateFlow(false)
-  override val isDeleted: StateFlow<Boolean> = _isDeleted
-
-  override fun deleteStop(stopId: String) {
-    _isDeleted.value = true
-  }
-}
-
 @RunWith(AndroidJUnit4::class)
-class StopListTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
+class StopItemTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
   @get:Rule val composeTestRule = createComposeRule()
 
-  @Before
-  fun setup() {
-    // Set up the test
+  private val stop =
+      Stop(
+          stopId = "1",
+          title = "Test Stop",
+          startTime = LocalTime.of(9, 0),
+          duration = 60,
+          date = LocalDate.of(2023, 5, 16),
+          address = "123 Test St",
+          budget = 100.0,
+          description = "This is a test stop.",
+          geoCords = GeoCords(37.7749, -122.4194))
 
+  private val tripsRepository = mockk<TripsRepository>(relaxed = true)
+
+  @Test
+  fun testStopItemDisplaysCorrectly() = run {
+    composeTestRule.setContent {
+      StopItem(stop = stop, tripId = "trip1", tripsRepository = tripsRepository, onDelete = {})
+    }
+
+    composeTestRule.onNodeWithTag("activityItemButton1", useUnmergedTree = true).assertExists()
+    composeTestRule
+        .onNodeWithTag("ActivityTitle1", useUnmergedTree = true)
+        .assertTextContains("Test Stop")
+    composeTestRule
+        .onNodeWithTag("ActivityTime1", useUnmergedTree = true)
+        .assertTextContains("09:00 - 10:00")
+    composeTestRule
+        .onNodeWithTag("ActivityAddress1", useUnmergedTree = true)
+        .assertTextContains("123 Test St")
   }
 
-  @Test fun testDeleteStop() {}
+  @Test
+  fun testDeleteButtonFunctionality() = run {
+    // Set the user as an admin
+    SessionManager.setUserSession(role = Role.ADMIN)
+    var deleteClicked = false
+    composeTestRule.setContent {
+      StopItem(
+          stop = stop,
+          tripId = "trip1",
+          tripsRepository = tripsRepository,
+          onDelete = { deleteClicked = true })
+    }
+
+    composeTestRule.onNodeWithTag("deleteButton1", useUnmergedTree = true).performClick()
+    // Wait for the dialog to be displayed
+    composeTestRule.waitForIdle()
+    // Assuming SessionManager.isAdmin() returns true for testing purpose
+    composeTestRule
+        .onNodeWithTag("confirmDeleteButton1", useUnmergedTree = true)
+        .assertExists()
+        .performClick()
+
+    composeTestRule.mainClock.advanceTimeBy(500) // Wait for the delete action to complete
+
+    assert(deleteClicked) { "Delete callback was not triggered" }
+  }
+
+  @Test
+  fun testDeleteCancelFunctionality() = run {
+    // Set the user as an admin
+    SessionManager.setUserSession(role = Role.ADMIN)
+
+    var deleteClicked = false
+    composeTestRule.setContent {
+      StopItem(
+          stop = stop,
+          tripId = "trip1",
+          tripsRepository = tripsRepository,
+          onDelete = { deleteClicked = true })
+    }
+    composeTestRule.onNodeWithTag("deleteButton1", useUnmergedTree = true).performClick()
+    // Wait for the dialog to be displayed
+    composeTestRule.waitForIdle()
+
+    // Assuming SessionManager.isAdmin() returns true for testing purpose
+    composeTestRule
+        .onNodeWithTag("cancelDeleteButton1", useUnmergedTree = true)
+        .assertExists()
+        .performClick()
+    composeTestRule.mainClock.advanceTimeBy(500) // Wait for the delete action to complete
+    assert(!deleteClicked) { "Delete callback was not triggered" }
+  }
+
+  @Test
+  fun testStopInfoDialogDisplayed() = run {
+    composeTestRule.setContent {
+      StopItem(stop = stop, tripId = "trip1", tripsRepository = tripsRepository, onDelete = {})
+    }
+
+    composeTestRule.onNodeWithTag("activityItemButton1").performClick()
+    composeTestRule.onNodeWithTag("activityDialog").assertExists()
+    composeTestRule.onNodeWithTag("titleText").assertTextContains("Test Stop")
+    composeTestRule.onNodeWithTag("activityDate").assertTextContains("Tuesday, 16/05/2023")
+    composeTestRule.onNodeWithTag("activitySchedule").assertTextContains("09:00 - 10:00")
+    composeTestRule.onNodeWithTag("activityAddress").assertTextContains("123 Test St")
+    composeTestRule.onNodeWithTag("activityBudget").assertTextContains("100.0")
+    composeTestRule.onNodeWithTag("activityDescription").assertTextContains("This is a test stop.")
+  }
 }
---
+++ app/src/test/java/com/github/se/wanderpals/viewmodel/StopItemViewModelTest.kt
@@ -0,0 +1,80 @@
+package com.github.se.wanderpals.viewmodel
+
+import com.github.se.wanderpals.model.repository.TripsRepository
+import com.github.se.wanderpals.model.viewmodel.StopItemViewModel
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class StopItemViewModelTest {
+
+  private lateinit var viewModel: StopItemViewModel
+  private lateinit var mockTripsRepository: TripsRepository
+  private val testDispatcher = StandardTestDispatcher()
+
+  private val tripId = "tripId"
+  private val stopId = "stopId"
+
+  @Before
+  fun setup() {
+    // Set the main dispatcher to a test dispatcher to control coroutine execution
+    Dispatchers.setMain(testDispatcher)
+
+    // Mock the TripsRepository to be used in the ViewModel
+    mockTripsRepository = mockk(relaxed = true)
+    setupMockResponses()
+
+    // Create the ViewModel using a factory with the mocked repository
+    val factory = StopItemViewModel.StopItemViewModelFactory(mockTripsRepository, tripId)
+    viewModel = factory.create(StopItemViewModel::class.java)
+  }
+
+  private fun setupMockResponses() {
+    // Define responses from the mocked repository to be returned when methods are called
+    coEvery { mockTripsRepository.removeStopFromTrip(tripId, stopId) } returns true
+  }
+
+  @Test
+  fun `deleteStop updates isDeleted state to true`() =
+      runTest(testDispatcher) {
+        // Call the deleteStop method
+        viewModel.deleteStop(stopId)
+
+        // Wait for all coroutines started during the test to complete
+        advanceUntilIdle()
+
+        // Verify that the stop was removed from the trip
+        coVerify { mockTripsRepository.removeStopFromTrip(tripId, stopId) }
+
+        // Assert that the isDeleted state was updated to true
+        assertTrue(viewModel.isDeleted.value)
+      }
+
+  @Test
+  fun `resetDeleteState updates isDeleted state to false`() {
+    // Set the isDeleted state to true
+    viewModel.resetDeleteState()
+
+    // Assert that the isDeleted state was updated to false
+    assertFalse(viewModel.isDeleted.value)
+  }
+
+  @After
+  fun tearDown() {
+    // Reset the main dispatcher to the original Main dispatcher after tests
+    Dispatchers.resetMain()
+  }
+}
