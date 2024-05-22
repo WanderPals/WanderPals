6b34c19ae287b62672ff37defa887c80f1408d67
Exino329 <126015847+Exino329@users.noreply.github.com>
2024-05-22 17:31:34 +0200

    added new currency param in documentation for CategoryContent.kt

--- app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/CategoryContent.kt
+++ app/src/main/java/com/github/se/wanderpals/ui/screens/trip/finance/CategoryContent.kt
@@ -38,91 +38,104 @@ import com.github.se.wanderpals.ui.PullToRefreshLazyColumn
  * others. The number of transaction and the total amount for each category is also displayed as
  * items below the pie-chart.
  *
- * @param innerPadding Padding values for the inner content
- * @param expenseList List of expenses
- * @param onRefresh Callback function for handling refresh action
+ * @param innerPadding Padding values for the inner content.
+ * @param expenseList List of expenses.
+ * @param onRefresh Callback function for handling refresh action.
+ * @param currencySymbol Symbol of the currency.
  */
 @Composable
 fun CategoryContent(
     innerPadding: PaddingValues,
     expenseList: List<Expense>,
     onRefresh: () -> Unit,
-    currencySymbol : String
+    currencySymbol: String
 ) {
 
-  /*Grouping expenses by category and calculating total number of transactions
-  and total amount for each category which are by default 0 if the category
-  has no associated expenses */
-  val categoryTransactionMap =
-      expenseList
-          .groupBy { it.category }
-          .mapValues { (_, expenses) ->
-            val nbPayments = expenses.size
-            val totalAmount = expenses.sumOf { it.amount }
-            Pair(nbPayments, totalAmount)
-          }
-          .withDefault { _ -> Pair(0, 0.0) }
-
-  val lazyColumn =
-      @Composable {
-        LazyColumn(
-            modifier =
-                Modifier.fillMaxWidth().padding(innerPadding).testTag("categoryOptionLazyColumn"),
-            contentPadding = PaddingValues(horizontal = 25.dp, vertical = 10.dp)) {
-
-              // Pie chart
-              item {
-                Surface(
-                    shape = RoundedCornerShape(20.dp),
-                    color = MaterialTheme.colorScheme.secondaryContainer,
-                    modifier = Modifier.testTag("categoryOptionPieChart"),
-                ) {
-                  Column(
-                      modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
-                      verticalArrangement = Arrangement.SpaceBetween,
-                      horizontalAlignment = Alignment.CenterHorizontally) {
-                        FinancePieChart(
-                            expenses = expenseList,
-                            radiusOuter = 80.dp,
-                            totalValueDisplayIsEnabled = true,
-                            currencySymbol = currencySymbol)
-
-                        Spacer(modifier = Modifier.height(20.dp))
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceEvenly) {
-                              PieChartCategoryText(category = Category.TRANSPORT)
-
-                              PieChartCategoryText(category = Category.ACCOMMODATION)
-
-                              PieChartCategoryText(category = Category.ACTIVITIES)
-                            }
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceEvenly) {
-                              PieChartCategoryText(category = Category.FOOD)
-
-                              PieChartCategoryText(category = Category.OTHER)
-                            }
-                      }
-                }
-              }
-
-              // Category information items
-              items(Category.values()) { category ->
-                val categoryInfo = categoryTransactionMap.getValue(category)
-                Spacer(modifier = Modifier.height(20.dp))
-                CategoryInfoItem(
-                    category = category,
-                    nbTransaction = categoryInfo.first,
-                    totalCategoryAmount = categoryInfo.second,
-                    currencySymbol = currencySymbol)
-              }
+    /*Grouping expenses by category and calculating total number of transactions
+    and total amount for each category which are by default 0 if the category
+    has no associated expenses */
+    val categoryTransactionMap =
+        expenseList
+            .groupBy { it.category }
+            .mapValues { (_, expenses) ->
+                val nbPayments = expenses.size
+                val totalAmount = expenses.sumOf { it.amount }
+                Pair(nbPayments, totalAmount)
             }
-      }
-  PullToRefreshLazyColumn(inputLazyColumn = lazyColumn, onRefresh = onRefresh)
+            .withDefault { _ -> Pair(0, 0.0) }
+
+    val lazyColumn =
+        @Composable {
+            LazyColumn(
+                modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(innerPadding)
+                    .testTag("categoryOptionLazyColumn"),
+                contentPadding = PaddingValues(horizontal = 25.dp, vertical = 10.dp)
+            ) {
+
+                // Pie chart
+                item {
+                    Surface(
+                        shape = RoundedCornerShape(20.dp),
+                        color = MaterialTheme.colorScheme.secondaryContainer,
+                        modifier = Modifier.testTag("categoryOptionPieChart"),
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 10.dp),
+                            verticalArrangement = Arrangement.SpaceBetween,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            FinancePieChart(
+                                expenses = expenseList,
+                                radiusOuter = 80.dp,
+                                totalValueDisplayIsEnabled = true,
+                                currencySymbol = currencySymbol
+                            )
+
+                            Spacer(modifier = Modifier.height(20.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                PieChartCategoryText(category = Category.TRANSPORT)
+
+                                PieChartCategoryText(category = Category.ACCOMMODATION)
+
+                                PieChartCategoryText(category = Category.ACTIVITIES)
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                PieChartCategoryText(category = Category.FOOD)
+
+                                PieChartCategoryText(category = Category.OTHER)
+                            }
+                        }
+                    }
+                }
+
+                // Category information items
+                items(Category.values()) { category ->
+                    val categoryInfo = categoryTransactionMap.getValue(category)
+                    Spacer(modifier = Modifier.height(20.dp))
+                    CategoryInfoItem(
+                        category = category,
+                        nbTransaction = categoryInfo.first,
+                        totalCategoryAmount = categoryInfo.second,
+                        currencySymbol = currencySymbol
+                    )
+                }
+            }
+        }
+    PullToRefreshLazyColumn(inputLazyColumn = lazyColumn, onRefresh = onRefresh)
 }
+
 /**
  * Composable function to display a row containing a colored box representing the category and the
  * name of the category.
@@ -131,16 +144,19 @@ fun CategoryContent(
  */
 @Composable
 fun PieChartCategoryText(category: Category) {
-  Row(verticalAlignment = Alignment.CenterVertically) {
-    Box(
-        modifier =
-            Modifier.size(10.dp)
-                .background(category.color, shape = RoundedCornerShape(size = 2.dp)))
-    Text(
-        text = category.nameToDisplay,
-        style = MaterialTheme.typography.bodyMedium,
-        modifier = Modifier.padding(start = 8.dp))
-  }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Box(
+            modifier =
+            Modifier
+                .size(10.dp)
+                .background(category.color, shape = RoundedCornerShape(size = 2.dp))
+        )
+        Text(
+            text = category.nameToDisplay,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
 }
 
 /**
@@ -150,59 +166,76 @@ fun PieChartCategoryText(category: Category) {
  * @param category The category for which the information is displayed.
  * @param nbTransaction The number of transactions in the category.
  * @param totalCategoryAmount The total amount spent in the category.
+ * @param currencySymbol Symbol of the currency.
  */
 @Composable
-fun CategoryInfoItem(category: Category, nbTransaction: Int, totalCategoryAmount: Double,currencySymbol: String) {
+fun CategoryInfoItem(
+    category: Category,
+    nbTransaction: Int,
+    totalCategoryAmount: Double,
+    currencySymbol: String
+) {
 
-  Surface(
-      modifier = Modifier.fillMaxWidth().height(60.dp).testTag(category.nameToDisplay + "InfoItem"),
-      shape = RoundedCornerShape(10.dp),
-      color = MaterialTheme.colorScheme.secondaryContainer,
-  ) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically) {
-          Row(
-              horizontalArrangement = Arrangement.SpaceBetween,
-              verticalAlignment = Alignment.CenterVertically) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .testTag(category.nameToDisplay + "InfoItem"),
+        shape = RoundedCornerShape(10.dp),
+        color = MaterialTheme.colorScheme.secondaryContainer,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Box(
                     modifier =
-                        Modifier.padding(start = 10.dp)
-                            .size(35.dp)
-                            .clip(RoundedCornerShape(10.dp))
-                            .background(category.color))
+                    Modifier
+                        .padding(start = 10.dp)
+                        .size(35.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(category.color)
+                )
 
                 Column {
-                  Text(
-                      modifier = Modifier.padding(start = 10.dp),
-                      text = category.nameToDisplay,
-                      style =
-                          MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                      color = MaterialTheme.colorScheme.primary,
-                  )
-                  Text(
-                      modifier =
-                          Modifier.padding(start = 10.dp)
-                              .testTag(category.nameToDisplay + "NbTransactions"),
-                      text =
-                          if (nbTransaction == 1) "$nbTransaction transaction"
-                          else "$nbTransaction transactions",
-                      style = MaterialTheme.typography.bodyMedium,
-                      color = Color.Gray,
-                  )
+                    Text(
+                        modifier = Modifier.padding(start = 10.dp),
+                        text = category.nameToDisplay,
+                        style =
+                        MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                    Text(
+                        modifier =
+                        Modifier
+                            .padding(start = 10.dp)
+                            .testTag(category.nameToDisplay + "NbTransactions"),
+                        text =
+                        if (nbTransaction == 1) "$nbTransaction transaction"
+                        else "$nbTransaction transactions",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray,
+                    )
                 }
-              }
+            }
 
-          Text(
-              modifier =
-                  Modifier.padding(end = 15.dp).testTag(category.nameToDisplay + "TotalAmount"),
-              text = "%.2f $currencySymbol".format(totalCategoryAmount),
-              style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
-              color = MaterialTheme.colorScheme.primary,
-              textAlign = TextAlign.End,
-              overflow = TextOverflow.Ellipsis,
-              maxLines = 1)
+            Text(
+                modifier =
+                Modifier
+                    .padding(end = 15.dp)
+                    .testTag(category.nameToDisplay + "TotalAmount"),
+                text = "%.2f $currencySymbol".format(totalCategoryAmount),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.End,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
         }
-  }
+    }
 }
