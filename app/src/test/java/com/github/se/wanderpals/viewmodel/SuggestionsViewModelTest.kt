9ff6efc7ce0ad719f48f2c36a151f9c44af39d0b
Gabin Forestier <gabin.forestier1007@gmail.com>
2024-05-23 20:47:28 +0200

    ktfmt

--- app/src/test/java/com/github/se/wanderpals/viewmodel/SuggestionsViewModelTest.kt
+++ app/src/test/java/com/github/se/wanderpals/viewmodel/SuggestionsViewModelTest.kt
@@ -132,7 +132,7 @@ class SuggestionsViewModelTest {
 
     coEvery { mockTripsRepository.addStopToTrip(any(), any()) } returns true
     coEvery { mockTripsRepository.removeSuggestionFromTrip(any(), any()) } returns true
-      coEvery { mockTripsRepository.getUserFromTrip(tripId, any()) } returns user1
+    coEvery { mockTripsRepository.getUserFromTrip(tripId, any()) } returns user1
   }
 
   // Example test for loading suggestions
@@ -179,7 +179,8 @@ class SuggestionsViewModelTest {
         advanceUntilIdle()
 
         // Assert the new comment is included and the comments count is correct
-        assertTrue(viewModel.state.value.first().comments.contains(newComment.copy(userName = "John Doe")))
+        assertTrue(
+            viewModel.state.value.first().comments.contains(newComment.copy(userName = "John Doe")))
         assertEquals(suggestion.comments.size + 1, viewModel.state.value.first().comments.size)
       }
 
